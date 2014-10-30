7c18431c2b446687f69b520bb564851a3d3a71f5
kwokholo.Macbook <kwokholo@uwm.edu>
2014-10-30 16:09:18 -0500

    kjbb

--- src/programAnalysis/ConstantPropagation.java
+++ src/programAnalysis/ConstantPropagation.java
@@ -55,17 +55,387 @@ class Int extends ExtInt {
 
 class Context {
 	// Define Context class to represent call strings of at most size <= k
-	// so if k == 1, then it should return the {9,4,6}
-	
+	// so if k == 1, then it should return the {7,9}
+	ArrayList<Label> callStringWhenKEqualsToOne;
 	int k;
-	CSet<k,> //TODO : Map k to each call string
 	public Context(int size){
+		callStringWhenKEqualsToOne = new ArrayList<Label>();
 		k = size;
-		
+	}
+	public Context(int size, Label s){
+		callStringWhenKEqualsToOne = new ArrayList<Label>();
+		k = size;
+		callStringWhenKEqualsToOne.add(s);
+	}
+	
+	public void addCallStringWhenKEqualsToOne(Label label){
+		callStringWhenKEqualsToOne.add(label);
 	}
 }
 
-// sigma: Var -> ExtInt (Z + top + bottom)
+
+
+class ContextualState extends HashMap<Context, State> {
+	private static final long serialVersionUID = 1L;
+	
+	final State theState;
+	ContextualState(State theState) { this.theState = theState; }	
+	ContextualState(ContextualState that) { super(that); this.theState = that.theState; }
+	
+	public State put(Context whatCallString, State s) {
+		return super.put(whatCallString,s);
+	}
+	
+	public State get(Object var) {
+		State ret = theState;
+		if (this.containsKey(var)) ret = super.get(var);
+		return ret;
+	}
+	
+	// not a legal variable name, we use it for function return value
+	private final static Context RET_STATE = new Context(1,new Label(0, null)); // k = 1
+	ContextualState setReturnState(State z) { 
+		ContextualState s = new ContextualState(this);
+		s.put(RET_STATE, z); 
+		return s;
+	}
+	State getReturnState() { return get(RET_STATE); }
+}
+
+
+
+public class ConstantPropagation2 extends InterDFA<ContextualState> {
+
+	@Override
+	ContextualState lub(ContextualState a, ContextualState u) {
+		State analysis = a.theState;
+		State update = u.theState;
+		
+		ExtInt defaultZ = analysis.getDefault().lub(update.getDefault());
+		State sigma; 
+		if (defaultZ == Top.getTop()) sigma = State.getTopState();
+		else sigma = State.getBottomState();
+		
+		List<String> vars = new ArrayList<String>();
+		vars.addAll(analysis.keySet());
+		vars.addAll(update.keySet());
+		
+		for(String v: vars) {
+			sigma.put(v, analysis.get(v).lub(update.get(v)));
+		}
+		 
+		return new ContextualState(sigma);
+	}
+
+	@Override
+	boolean lessThan(ContextualState u, ContextualState a) {
+		State analysis = a.theState;
+		State update = u.theState;
+		
+		
+		boolean ret = true;
+		
+		List<String> vars = new ArrayList<String>();
+		vars.addAll(analysis.keySet());
+		vars.addAll(update.keySet());
+		
+		for(String v: vars) {
+			
+			if (!update.get(v)
+					.lessThan(
+					analysis.get(v))) {
+				ret = false; break;
+			}
+		}
+		ret = ret && update.defaultZ.lessThan(analysis.defaultZ);
+		return ret;
+	}
+	
+	ConstantPropagation2(Statement entry) {
+		CFG cfg = new CFG();
+		entry.accept(new LabelVisitor(cfg));
+		entry.accept(new CFGVisitor(cfg));
+		
+		CP_Visitor cpv = new CP_Visitor(cfg);
+		entry.accept(cpv);
+		 
+		this.f = cpv.f;
+		
+		cfg.setLabels();
+		this.labels = cfg.f_labels.get(entry);
+		
+		this.flow = cfg.f_flow.get(entry);
+		this.bottom = State.getBottomState();
+		 
+		this.extremal_labels = new CSet<Label>(cfg.f_init.get(entry));
+		this.extremal_value = State.getTopState();
+	}
+	
+	
+}
+
+interface Function<Arg, Res> { Res apply(Arg arg); }
+interface Function2<Arg, Arg2, Res> { Function<Arg2, Res> apply(Arg arg); }
+interface Function3<Arg, Arg2, Arg3, Res> { Function2<Arg2, Arg3, Res> apply(Arg arg); }
+
+
+//CP_Fun_hat : (Lab -> ContextualState) -> ContextualState
+abstract class CP_Fun_hat implements TF_Function<ContextualState> {
+	 
+	//A_CP: AExp -> ContextualState -> State -> ExtInt
+	Function3<Expression,ContextualState, State, ExtInt> a_cp = new Function3<Expression, ContextualState, State, ExtInt>() {
+		public Function2<ContextualState,State, ExtInt> apply(final Expression exp) {
+			return new Function2<ContextualState, State, ExtInt>() {
+				public Function<State, ExtInt> apply(final ContextualState cs) {
+					return new Function<State, ExtInt>() {
+						public ExtInt apply(State sigma) {
+							A_CP_Visitor v = new A_CP_Visitor(sigma);
+							exp.accept(v);
+							return v.A_CP.get(exp);
+						}
+					};
+				}
+			};
+		}
+	};
+	
+	static CP_Fun_hat getIdFunction(final Label ell) {
+		return new CP_Fun_hat() {
+			public ContextualState apply(Analysis<ContextualState> arg) { return arg.get(ell); }
+		};
+	}
+	
+	
+	static CP_Fun_hat getReturnFunction(final Label ell, final Expression exp) {
+		return new CP_Fun_hat() {
+			public ContextualState apply(Analysis<ContextualState> analysis) {
+				return analysis.g
+				return analysis.get(ell).setReturnVar(a_cp.apply(exp).apply(analysis.get(ell)));
+			}
+		};
+	}
+	static CP_Fun_hat getVarDecFunction(final Label ell, final String var) {
+		return new CP_Fun_hat() {
+			public ContextualState apply(Analysis<ContextualState> analysis) {
+				return analysis.get(ell).update(context,var, Top.getTop());
+			}
+		};
+	}
+	static CP_Fun_hat getAssignFunction(final Context ct, final Label ell, final String var, final Expression exp) {
+		return new CP_Fun_hat() {
+			public ContextualState apply(Analysis<ContextualState> analysis) {
+				return analysis.get(ell).update(context,var, a_cp.apply(exp).apply(analysis.get(ell)));
+			}
+		};
+	}
+	static CP_Fun_hat getCallFunction(final Context ct, final Label ell, final List<Expression> args, final FunctionDec functionDec) {
+		return new CP_Fun_hat() {
+			public ContextualState apply(Analysis<ContextualState> analysis) {
+				List<String> params = functionDec.function.parameters;
+				State sigma = analysis.get(ell);
+				List<ExtInt> argsZ = new ArrayList<ExtInt>();
+				for(Expression a : args) argsZ.add(a_cp.apply(a).apply(sigma));
+				
+				return sigma.update(params, argsZ);
+			}
+		};
+	}
+	static CP_Fun_hat getCallRetFunction(final Context ct, final Label l_c, final Label l_r, final String lhs,
+			final FunctionDec functionDec, final List<String> localVars) {
+		
+		return new CP_Fun_hat() {
+			public ContextualState apply(Analysis<ContextualState> analysis) {
+				List<String> params = functionDec.function.parameters;
+				localVars.addAll(params);
+				 
+				return analysis.get(l_r).update(context, lhs, localVars, analysis.get(l_c));
+			}
+		};
+	}
+}
+
+
+
+class CP_Visitor_Context_Sen extends WhileLangVisitor {
+	// constant propagation's transfer functions: Lab -> CP_Function
+	Map<Label, TF_Function<ContextualState>> f = new HashMap<Label, TF_Function<ContextualState>>();
+	
+	CFG cfg;
+	
+	CP_Visitor_Context_Sen(CFG cfg) {
+		this.cfg = cfg;
+	}
+	
+	public void visit(BlockStmt s) { 
+		for(Statement stmt: s.statements) stmt.accept(this); 
+	}
+	public void visit(EmptyStmt s) { f.put(s.label, CP_Fun_hat.getIdFunction(s.label));  }
+	
+	// regular assignment: x = e
+	// function call x = f(e)
+	public void visit(ExpressionStmt s) {   
+		if (this.isAssignment(s)) {
+			final AssignmentExpr a = (AssignmentExpr) s.expr;
+			final Context ct = new Context(1,s.label);
+			
+			// make sure lhs of a is a variable
+			assertLValueVar(a.lValue, a);
+			final String x = ((VarAccessExpr) a.lValue).name;
+			
+			 if (this.isFunctionCallExpr(a)) {
+				 final FunctionCallExpr c = (FunctionCallExpr) a.rValue;
+				 
+				 Label l_n = cfg.get_l_n(s.label);
+				 if (l_n == null) {
+					 log(ERROR, "called function is not declared", s);
+				 }
+				 FunctionDec functionDec = (FunctionDec) l_n.stmt;
+		
+				 f.put(s.label, CP_Fun_hat.getCallFunction(ct,s.label, c.arguments, functionDec));
+				 CSet<VarDecStmt> varDecs = cfg.varDecs(functionDec.function.body);
+				 List<String> vars = new ArrayList<String>();
+				 for(VarDecStmt vd : varDecs) vars.add(vd.variable);
+				 
+				 f.put(s.label2, CP_Fun_hat.getCallRetFunction(ct,s.label, s.label2, x, functionDec, vars));
+			 } 
+			 // rhs is anything but any function call
+			 else {
+				 f.put(s.label, CP_Fun_hat.getAssignFunction(ct,s.label, x, a.rValue));
+			 }
+			
+			
+		}
+	}
+
+	public void visit(WhileStmt s) {  
+		f.put(s.label, CP_Fun_hat.getIdFunction(s.label));
+		s.body.accept(this);
+	}
+	public void visit(IfStmt s) { 
+		f.put(s.label, CP_Fun_hat.getIdFunction(s.label));
+		s.thenPart.accept(this);
+		if (s.hasElse()) s.elsePart.accept(this);
+	}
+	
+	// function declaration
+	public void visit(FunctionDec s) {  
+		s.function.body.accept(this);
+		
+		f.put(s.label, CP_Fun_hat.getIdFunction(s.label));
+		f.put(s.label2, CP_Fun_hat.getIdFunction(s.label2));
+	}
+	
+	public void visit(VarDecStmt s) {
+		f.put(s.label, CP_Fun_hat.getVarDecFunction(s.label, s.variable));
+	}
+	public void visit(ReturnStmt s) {   
+		f.put(s.label, CP_Fun_hat.getReturnFunction(s.label, s.expr));
+	}
+}
+
+// State -> AExp -> ExtInt
+class A_CP_Visitor extends WhileLangVisitor {
+	private State sigma;
+	Map<Expression, ExtInt> A_CP = new HashMap<Expression, ExtInt>();
+	
+	A_CP_Visitor (State sigma) {
+		this.sigma = sigma;
+	}
+	
+	public void visit(AssignmentExpr e) { 
+		e.rValue.accept(this);
+		A_CP.put(e, A_CP.get(e.rValue));
+	}
+	public void visit(FunctionCallExpr e) { log(ERROR, NO_evaluation, e); }
+	public void visit(VarAccessExpr e) { 
+		ExtInt i = Bottom.getBottom();
+		
+		String v = e.name;
+		if (sigma.containsKey(v)) {
+			i = sigma.get(v);
+		} // if a variable is not in sigma, we return bottom 
+		
+		A_CP.put(e, i);
+	}
+	public void visit(NumberExpr e) { 
+		A_CP.put(e, new Int((int) (e.number)));
+	}
+	public void visit(AddExpr e) { 
+		e.left.accept(this);
+		e.right.accept(this);
+		ExtInt il = A_CP.get(e.left), 
+			   ir = A_CP.get(e.right);
+	
+		A_CP.put(e, acp(Token.ADD, il, ir));
+	}
+	public void visit(NumericExpr e) { 
+		e.left.accept(this);
+		e.right.accept(this);
+		ExtInt il = A_CP.get(e.left), 
+			   ir = A_CP.get(e.right);
+	
+		A_CP.put(e, acp(e.op, il, ir));
+	}
+	public void visit(BoolExpr e) { log(ERROR, NO_evaluation, e); }
+	public void visit(LogicExpr e) { log(ERROR, NO_evaluation, e); }
+	public void visit(ComparisonExpr e) { log(ERROR, NO_evaluation, e); }
+	public void visit(NegationExpr e) { 
+		e.operand.accept(this);
+		A_CP.put(e, A_CP.get(e.operand));
+	}
+	
+	private static ExtInt acp(int op, ExtInt il, ExtInt ir) {
+		ExtInt ret;
+		if (il.isBottom() || ir.isBottom()) ret = Bottom.getBottom();
+		else if (il.isTop() || ir.isTop()) ret = Top.getTop();
+		else {
+			int l = ((Int) il).i, r = ((Int) ir).i;
+			int z = 0;
+			// only consider the following cases, otherwise, we default to 0
+			switch (op) {
+			case Token.SUB: z = l-r; break; 
+			case Token.MUL: z = l*r; break;
+			case Token.DIV: z = l/r; break;
+			case Token.ADD: z = l+r; break;
+			case Token.MOD: z = l%r; break;
+			case Token.BITOR: z = l|r; break; 
+			case Token.BITXOR: z = l^r; break;
+			case Token.BITAND: z = l&r; break;
+			case Token.LSH: z = l<<r; break;
+			case Token.RSH: z = l>>r; break;
+			case Token.URSH: z = l>>>r; break;
+			}
+			ret = new Int(z);
+		}
+		return ret;
+	}
+}
+
+
+
+
+/*
+ * 
+ * 
+ * 
+ * 
+ ************************** ORIGINAL CODE************************
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+//sigma: Var -> ExtInt (Z + top + bottom)
 class State extends HashMap<String, ExtInt> {
 	private static final long serialVersionUID = 1L;
 	
@@ -113,33 +483,6 @@ class State extends HashMap<String, ExtInt> {
 		}
 		return s;
 	}
-	
-	public ExtInt put(String var, ExtInt z) {
-		return super.put(var, z);
-	}
-	
-	public ExtInt get(Object var) {
-		ExtInt ret = defaultZ;
-		if (this.containsKey(var)) ret = super.get(var);
-		return ret;
-	}
-	
-	// not a legal variable name, we use it for function return value
-	private final static String RET_VAR = "0__ret"; 
-	State setReturnVar(ExtInt z) { 
-		State s = new State(this);
-		s.put(RET_VAR, z); 
-		return s;
-	}
-	ExtInt getReturnVar() { return get(RET_VAR); }
-}
-
-class ContextualState extends HashMap<Context, State> {
-	private static final long serialVersionUID = 1L;
-	
-	final Context callString;
-	private ContextualState(Context callString) { this.callString = callString; }	
-	
 	
 	public ExtInt put(String var, ExtInt z) {
 		return super.put(var, z);
@@ -224,8 +567,6 @@ public class ConstantPropagation extends InterDFA<State> {
 	
 }
 
-interface Function<Arg, Res> { Res apply(Arg arg); }
-interface Function2<Arg, Arg2, Res> { Function<Arg2, Res> apply(Arg arg); }
 
 //CP_Fun : (Lab -> State) -> State
 abstract class CP_Fun implements TF_Function<State> {
@@ -380,85 +721,5 @@ class CP_Visitor extends WhileLangVisitor {
 	}
 	public void visit(ReturnStmt s) {   
 		f.put(s.label, CP_Fun.getReturnFunction(s.label, s.expr));
-	}
-}
-
-
-
-// State -> AExp -> ExtInt
-class A_CP_Visitor extends WhileLangVisitor {
-	private State sigma;
-	Map<Expression, ExtInt> A_CP = new HashMap<Expression, ExtInt>();
-	
-	A_CP_Visitor (State sigma) {
-		this.sigma = sigma;
-	}
-	
-	public void visit(AssignmentExpr e) { 
-		e.rValue.accept(this);
-		A_CP.put(e, A_CP.get(e.rValue));
-	}
-	public void visit(FunctionCallExpr e) { log(ERROR, NO_evaluation, e); }
-	public void visit(VarAccessExpr e) { 
-		ExtInt i = Bottom.getBottom();
-		
-		String v = e.name;
-		if (sigma.containsKey(v)) {
-			i = sigma.get(v);
-		} // if a variable is not in sigma, we return bottom 
-		
-		A_CP.put(e, i);
-	}
-	public void visit(NumberExpr e) { 
-		A_CP.put(e, new Int((int) (e.number)));
-	}
-	public void visit(AddExpr e) { 
-		e.left.accept(this);
-		e.right.accept(this);
-		ExtInt il = A_CP.get(e.left), 
-			   ir = A_CP.get(e.right);
-	
-		A_CP.put(e, acp(Token.ADD, il, ir));
-	}
-	public void visit(NumericExpr e) { 
-		e.left.accept(this);
-		e.right.accept(this);
-		ExtInt il = A_CP.get(e.left), 
-			   ir = A_CP.get(e.right);
-	
-		A_CP.put(e, acp(e.op, il, ir));
-	}
-	public void visit(BoolExpr e) { log(ERROR, NO_evaluation, e); }
-	public void visit(LogicExpr e) { log(ERROR, NO_evaluation, e); }
-	public void visit(ComparisonExpr e) { log(ERROR, NO_evaluation, e); }
-	public void visit(NegationExpr e) { 
-		e.operand.accept(this);
-		A_CP.put(e, A_CP.get(e.operand));
-	}
-	
-	private static ExtInt acp(int op, ExtInt il, ExtInt ir) {
-		ExtInt ret;
-		if (il.isBottom() || ir.isBottom()) ret = Bottom.getBottom();
-		else if (il.isTop() || ir.isTop()) ret = Top.getTop();
-		else {
-			int l = ((Int) il).i, r = ((Int) ir).i;
-			int z = 0;
-			// only consider the following cases, otherwise, we default to 0
-			switch (op) {
-			case Token.SUB: z = l-r; break; 
-			case Token.MUL: z = l*r; break;
-			case Token.DIV: z = l/r; break;
-			case Token.ADD: z = l+r; break;
-			case Token.MOD: z = l%r; break;
-			case Token.BITOR: z = l|r; break; 
-			case Token.BITXOR: z = l^r; break;
-			case Token.BITAND: z = l&r; break;
-			case Token.LSH: z = l<<r; break;
-			case Token.RSH: z = l>>r; break;
-			case Token.URSH: z = l>>>r; break;
-			}
-			ret = new Int(z);
-		}
-		return ret;
 	}
 }
